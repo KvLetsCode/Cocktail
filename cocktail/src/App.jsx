460a4f6e2873cba95392d4231a078274d9521e9b
kv <kvdata9@gmail.com>
2025-08-22 15:03:38 +0530

    About section

--- cocktail/src/App.jsx
+++ cocktail/src/App.jsx
@@ -6,6 +6,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktail from './components/Cocktail';
+import About from './components/About';
 
 function App() {
   
@@ -16,6 +17,7 @@ function App() {
         <Navbar />
         <Hero />
         <Cocktail />
+        <About />
       </main>
       
     </>
